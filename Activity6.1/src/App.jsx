d74d2ea77f946ec67b90680bdd658b7277590185
sowmya1293 <33096416+sowmya1293@users.noreply.github.com>
2025-12-06 22:20:19 +0530

    Add files via upload

---
+++ Activity6.1/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [cssInput, setCssInput] = useState("");
+  const [cssLabel, setCssLabel] = useState("");
+
+  function checkEmail(event) {
+    setEmailId(event.target.value);
+  }
+
+  function checkPassword(event) {
+    setPassword(event.target.value);
+  }
+
+  function checkValidity() {
+    if (
+      !emailId.includes("@") ||
+      !emailId.includes(".com") ||
+      password.length < 8
+    ) {
+      setCssInput("error-input");
+      setCssLabel("error-label");
+    } else {
+      setCssInput("");
+      setCssLabel("");
+    }
+    event.preventDefault();
+  }
+
+  return (
+    <>
+      {/* <form className={`email-form ${cssError}`}> */}
+      <form className="email-form">
+        <div className="email-div input-div">
+          <label htmlFor="email" className={cssLabel}>
+            Your email
+          </label>
+          <input
+            type="text"
+            id="email"
+            className={cssInput}
+            onChange={checkEmail}
+          />
+        </div>
+        <div className="password-div input-div">
+          <label htmlFor="password" className={cssLabel}>
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className={cssInput}
+            onChange={checkPassword}
+          />
+        </div>
+        <button type="submit" onClick={checkValidity}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default App;
